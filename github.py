8ee889b127bcb6df7e8345754fcce2b318e4cb1f
jbt <benjaminemye16@gmail.com>
2024-09-08 13:02:37 +0300

    Tirgul2

--- github.py
+++ github.py
@@ -2,7 +2,7 @@
 print(" Hello Enter your  name: lin ")
 
 # 2
-print(" Hello Enter your adress: beit alfa")
+print(" Hello Enter your animal: dog")
 
 # name = "Lin"
 # print(" Hello ", name, " How are you: ")
